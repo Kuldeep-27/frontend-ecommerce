5ba44f6c47be99539dac4ac0eededbff5f318e84
Kuldeeep Khutela <khutelakuldeep52@gmail.com>
2024-07-11 15:24:49 +0530

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ import Search from "./components/Search/Search";
 
 const App = () => {
   return (
-    <div style={{marginTop:"10vh"}}>
+    <div style={{marginTop:"13vh"}}>
 
       <Toaster/>
     
